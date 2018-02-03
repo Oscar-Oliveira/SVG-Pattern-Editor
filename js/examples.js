894d7be59b46036ffdb13e86a39e71ba2b36c42e
Oscar Oliveira <oscar.m.oliveira@gmail.com>
2018-02-03 14:06:29 +0000

    First Commit

---
+++ js/examples.js
@@ -0,0 +1,47 @@
+var example1 = '<svg id="pattern" width="300" height="300" x="0" y="0">\n\
+    <rect x="1" y="1" width="80" height="40" stroke="black" stroke-width="1px" fill="white"/>\n\
+    <rect x="1" y="1" width="40" height="40" stroke="black" stroke-width="1px" fill="white"/>\n\
+</svg>'
+
+var example2 = '<svg id="PATTERN" width="689" height="459.3" x="0" y="0" viewBox="0 0 689 459.3">\n \
+<rect width="689" height="459.3" stroke="black" stroke-width="1px" fill="gray"/>\n \
+<svg width="689" height="459.3" x="0" y="0">\n \
+    <rect width="689" height="459.3" stroke="black" stroke-width="1px" fill="gray"/>\n \
+    <svg width="689" height="57.4" x="0" y="0">\n \
+        <rect width="689" height="57.4" stroke="black" stroke-width="1px" fill="lightgray" id="svgItem0">\n \
+            <title>120x10</title>\n \
+        </rect>\n \
+    </svg>\n \
+    <svg width="689" height="229.6" x="0" y="57.4">\n \
+        <rect width="689" height="229.6" stroke="black" stroke-width="1px" fill="gray"/>\n \
+        <svg width="459.3" height="229.6" x="0" y="0">\n \
+            <rect width="459.3" height="229.6" stroke="black" stroke-width="1px" fill="lightgray" id="svgItem1">\n \
+                <title>80x40</title>\n \
+            </rect>\n \
+        </svg>\n \
+        <svg width="57.4" height="229.6" x="459.3" y="0">\n \
+            <rect width="57.4" height="229.6" stroke="black" stroke-width="1px" fill="lightgray" id="svgItem2">\n \
+                <title>10x40</title>\n \
+            </rect>\n \
+        </svg>\n \
+        <svg width="172.25" height="229.6" x="516.75" y="0">\n \
+            <rect width="172.25" height="229.6" stroke="black" stroke-width="1px" fill="lightgray" id="svgItem3">\n \
+                <title>30x40</title>\n \
+            </rect>\n \
+        </svg>\n \
+    </svg>\n \
+    <svg width="689" height="172.25" x="0" y="287.0833333333333">\n \
+        <rect width="689" height="172.25" stroke="black" stroke-width="1px" fill="gray"/>\n \
+        <svg width="344.5" height="172.25" x="0" y="0">\n \
+            <rect width="344.5" height="172.25" stroke="black" stroke-width="1px" fill="lightgray" id="svgItem4">\n \
+                <title>60x30</title>\n \
+            </rect>\n \
+        </svg>\n \
+        <svg width="229.6" height="172.25" x="344.5" y="0">\n \
+            <rect width="229.6" height="172.25" stroke="black" stroke-width="1px" fill="lightgray" id="svgItem5">\n \
+                <title>40x30</title>\n \
+            </rect>\n \
+        </svg>\n \
+    </svg>\n \
+</svg>\n \
+</svg>'	
